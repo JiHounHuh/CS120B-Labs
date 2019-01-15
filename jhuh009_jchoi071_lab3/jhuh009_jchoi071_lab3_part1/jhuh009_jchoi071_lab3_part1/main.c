236abf76bed614772a2b665536c40084d4a8a196
JiHounHuh <PatrickWumbo@users.noreply.github.com>
2019-01-15 14:26:33 -0800

    exercise 1

--- jhuh009_jchoi071_lab3/jhuh009_jchoi071_lab3_part1/jhuh009_jchoi071_lab3_part1/main.c
+++ jhuh009_jchoi071_lab3/jhuh009_jchoi071_lab3_part1/jhuh009_jchoi071_lab3_part1/main.c
@@ -18,37 +18,42 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF; //Configure port A's 8 pins as inputs, initialize to 1s
 	DDRB = 0x00; PORTB = 0xFF; // Configure port B's 8 pins as inputs, initialize to 1s
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
-	unsigned char tmpA = 0x00;
-	unsigned char tmpB = 0x00;
-	unsigned char buttonA = 0x00;
-	unsigned char buttonB = 0x00;
-	unsigned char counter = 0x00;
+// 	unsigned char tmpA = 0x00;
+// 	unsigned char tmpB = 0x00;
+// 	unsigned char buttonA = 0x00;
+// 	unsigned char buttonB = 0x00;
+	unsigned short counter = 0x00;
 	unsigned char place = 0x00;
 	while(1) 
 	{
 		
-		buttonA = PINA & 0x01;
-		buttonB = PINB & 0x01;
+// 		buttonA = PINA & 0x01;
+// 		buttonB = PINB & 0x01;
 // 		PINA >> 1;
 // 		PINB >> 1;
 // 		if(counter < 8)
 // 		{
-		if(GetBit(buttonA, counter)) {
-			place++;
-			tmpA = SetBit(tmpA,1,0);
-			tmpA = SetBit(tmpA,0,1);
-			
+		for(counter = 0; counter < 8; counter++)
+		{
+			if(GetBit(PINA, counter)) {
+				place++;
+				//tmpA = SetBit(tmpA,1,0);
+				//tmpA = SetBit(tmpA,0,1);
+				
+			}
+			if(GetBit(PINB,counter)) {
+				place++;
+				// 			tmpB = SetBit(tmpB, 1, 0);
+				// 			tmpB = SetBit(tmpB, 0, 1);
+				
+			}
+			PORTC = place;	
 		}
-		if(GetBit(buttonB,counter)) {
-			place++;
-			tmpB = SetBit(tmpB, 1, 0);
-			tmpB = SetBit(tmpB, 0, 1);
-			
-		}
-		counter++;
+		
 		//PINA >> 1;
 		//PINB >> 1;
-		PORTC = place;	
+		
+		place = 0;
 			
 		//}
 		
