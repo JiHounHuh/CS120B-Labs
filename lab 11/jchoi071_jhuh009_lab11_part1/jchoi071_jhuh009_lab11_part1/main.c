e132715c4abc7c03d8f744246594bc5f32751d94
JiHounHuh <PatrickWumbo@users.noreply.github.com>
2019-02-21 15:30:11 -0800

    part 1

--- lab 11/jchoi071_jhuh009_lab11_part1/jchoi071_jhuh009_lab11_part1/main.c
+++ lab 11/jchoi071_jhuh009_lab11_part1/jchoi071_jhuh009_lab11_part1/main.c
@@ -6,53 +6,10 @@
  */ 
 
 #include <avr/io.h>
-#include <ucr/bit.h>
-
-// Returns '\0' if no key pressed, else returns char '1', '2', ... '9', 'A', ...
-// If multiple keys pressed, returns leftmost-topmost one
-// Keypad must be connected to port C
-/* Keypad arrangement
-        PC4 PC5 PC6 PC7
-   col  1   2   3   4
-row
-PC0 1   1 | 2 | 3 | A
-PC1 2   4 | 5 | 6 | B
-PC2 3   7 | 8 | 9 | C
-PC3 4   * | 0 | # | D
-*/
-unsigned char GetKeypadKey() {
-
-	PORTC = 0xEF; // Enable col 4 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('1'); }
-	if (GetBit(PINC,1)==0) { return('4'); }
-	if (GetBit(PINC,2)==0) { return('7'); }
-	if (GetBit(PINC,3)==0) { return('*'); }
-
-	// Check keys in col 2
-	PORTC = 0xDF; // Enable col 5 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	if (GetBit(PINC,0)==0) { return('2'); }
-	// ... *****FINISH*****
-
-	// Check keys in col 3
-	PORTC = 0xBF; // Enable col 6 with 0, disable others with 1’s
-	asm("nop"); // add a delay to allow PORTC to stabilize before checking
-	// ... *****FINISH*****
-
-	// Check keys in col 4	
-	// ... *****FINISH*****
-
-	return('\0'); // default value
-
-}
-
-
-#include <avr/io.h>
 #include <avr/interrupt.h>
-#include <ucr/bit.h>
-#include <ucr/timer.h>
-#include <stdio.h>
+#include "bit.h"
+#include "timer.h"
+#include "stdio.h"
 
 //--------Find GCD function --------------------------------------------------
 unsigned long int findGCD(unsigned long int a, unsigned long int b)
@@ -62,11 +19,12 @@ unsigned long int findGCD(unsigned long int a, unsigned long int b)
 		c = a%b;
 		if(c==0){return b;}
 		a = b;
-b = c;
+		b = c;
 	}
 	return 0;
 }
 //--------End find GCD function ----------------------------------------------
+
 
 //--------Task scheduler data structure---------------------------------------
 // Struct for Tasks represent a running process in our simple real-time operating system.
@@ -97,21 +55,69 @@ enum SM1_States { SM1_wait, SM1_press, SM1_release };
 int SMTick1(int state) {
 
 	// Local Variables
-	unsigned char press = ~PINA & 0x01;
+	unsigned char press = ~PINC;
 
 	//State machine transitions
 	switch (state) {
-	case SM1_wait: 	if (press == 0x01) {	// Wait for button press
-state = SM1_press;
-}
-break;
+	case SM1_wait: 	
+		if (press == 0x01) {	// Input 1
+			state = SM1_press;
+		}
+		if (press == 0x02) {	// Input 2
+			state = SM1_press;
+		}
+// 		if (press == 0x03) {	// Input 3
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x04) {	// Input 4
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x05) {	// Input 5
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x06) {	// Input 6
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x07) {	// Input 7
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x08) {	// Input 8
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x09) {	// Input 9
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x0A) {	// Input A
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x0B) {	// Input B
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x0C) {	// Input C
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x0D) {	// Input D
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x0E) {	// Input #
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x0F) {	// Input 0
+// 			state = SM1_press;
+// 		}
+// 		if (press == 0x10) {	// Input *
+// 			state = SM1_press;
+// 		}
+		break;
 
-	case SM1_press:	state = SM1_release;
-break;
+	case SM1_press:	
+		state = SM1_release;
+		break;
 
-	case SM1_release:	if (press == 0x00) {	// Wait for button release
-state = SM1_wait;
-}
+	case SM1_release:	
+		if (press == 0x00) {	// Wait for button release
+	state = SM1_wait;
+	}
 break;
 
 	default:		state = SM1_wait; // default: Initial state
@@ -120,10 +126,12 @@ break;
 
 	//State machine actions
 	switch(state) {
-	case SM1_wait:	break;
+	case SM1_wait:	
+			PORTB = press;
+		
+		break;
 
-	case SM1_press:	pause = (pause == 0) ? 1 : 0; // toggle pause
-break;
+	case SM1_press:	break;
 
 	case SM1_release:	break;
 
@@ -133,117 +141,12 @@ break;
 	return state;
 }
 
-//Enumeration of states.
-enum SM2_States { SM2_wait, SM2_blink };
-
-// If paused: Do NOT toggle LED connected to PB0
-// If unpaused: toggle LED connected to PB0
-int SMTick2(int state) {
-
-	//State machine transitions
-	switch (state) {
-	case SM2_wait:	if (pause == 0) {	// If unpaused, go to blink state
-state = SM2_blink;
-}
-break;
-
-	case SM2_blink:	if (pause == 1) {	// If paused, go to wait state
-state = SM2_wait;
-}
-break;
-
-	default:		state = SM2_wait;
-break;
-	}
-
-	//State machine actions
-	switch(state) {
-	case SM2_wait:	break;
-
-	case SM2_blink:	SM2_output = (SM2_output == 0x00) ? 0x01 : 0x00; //toggle LED
-break;
-
-	default:		break;
-	}
-
-	return state;
-}
-
-//Enumeration of states.
-enum SM3_States { SM3_wait, SM3_blink };
-
-// If paused: Do NOT toggle LED connected to PB1
-// If unpaused: toggle LED connected to PB1
-int SMTick3(int state) {
-	//State machine transitions
-	switch (state) {
-	case SM3_wait:	if (pause == 0) {	// If unpaused, go to blink state
-state = SM3_blink;
-}
-break;
-
-	case SM3_blink:	if (pause == 1) {	// If paused, go to wait state
-state = SM3_wait;
-}
-break;
-
-	default:		state = SM3_wait;
-				break;
-	}
-
-	//State machine actions
-	switch(state) {
-	case SM3_wait:	break;
-
-	case SM3_blink:	SM3_output = (SM3_output == 0x00) ? 0x02 : 0x00; //toggle LED
-				break;
-
-	default:		break;
-	}
-
-	return state;
-}
-
-
-//Enumeration of states.
-enum SM4_States { SM4_display };
-
-// Combine blinking LED outputs from SM2 and SM3, and output on PORTB
-int SMTick4(int state) {
-	// Local Variables
-
-	unsigned char output;
-
-	//State machine transitions
-	switch (state) {
-	case SM4_display:	break;
-
-	default:		state = SM4_display;
-				break;
-	}
-
-	//State machine actions
-	switch(state) {
-	case SM4_display:	output = SM2_output | SM3_output; // write shared outputs
-									// to local variables
-break;
-
-	default:		break;
-	}
-
-	PORTB = output;	// Write combined, shared output variables to PORTB
-
-	return state;
-}
-
-// --------END User defined FSMs-----------------------------------------------
-
 // Implement scheduler code from PES.
 int main()
 {
 // Set Data Direction Registers
 // Buttons PORTA[0-7], set AVR PORTA to pull down logic
-DDRA = 0x00; PORTA = 0xFF;
+DDRC = 0x00; PORTA = 0xFF;
 DDRB = 0xFF; PORTB = 0x00;
 // . . . etc
 
@@ -279,24 +182,6 @@ task1.period = SMTick1_period;//Task Period.
 task1.elapsedTime = SMTick1_period;//Task current elapsed time.
 task1.TickFct = &SMTick1;//Function pointer for the tick.
 
-// Task 2
-task2.state = -1;//Task initial state.
-task2.period = SMTick2_period;//Task Period.
-task2.elapsedTime = SMTick2_period;//Task current elapsed time.
-task2.TickFct = &SMTick2;//Function pointer for the tick.
-
-// Task 3
-task3.state = -1;//Task initial state.
-task3.period = SMTick3_period;//Task Period.
-task3.elapsedTime = SMTick3_period; // Task current elasped time.
-task3.TickFct = &SMTick3; // Function pointer for the tick.
-
-// Task 4
-task4.state = -1;//Task initial state.
-task4.period = SMTick4_period;//Task Period.
-task4.elapsedTime = SMTick4_period; // Task current elasped time.
-task4.TickFct = &SMTick4; // Function pointer for the tick.
-
 // Set the timer and turn it on
 TimerSet(GCD);
 TimerOn();
@@ -321,32 +206,3 @@ while(1) {
 // Error: Program should not exit!
 return 0;
 }
-
-
-
-
-
-int main(void)
-{
-	unsigned char x;
-	DDRB = 0xFF; PORTB = 0x00; // PORTB set to output, outputs init 0s
-	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
-	while(1) {
-		x = GetKeypadKey();
-		switch (x) {
-			case '\0': PORTB = 0x1F; break; // All 5 LEDs on
-			case '1': PORTB = 0x01; break; // hex equivalent
-			case '2': PORTB = 0x02; break;
-
-			// . . . ***** FINISH *****
-
-			case 'D': PORTB = 0x0D; break;
-			case '*': PORTB = 0x0E; break;
-			case '0': PORTB = 0x00; break;
-			case '#': PORTB = 0x0F; break;
-			default: PORTB = 0x1B; break; // Should never occur. Middle LED off.
-		}
-	}
-}
-
-
